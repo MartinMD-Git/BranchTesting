ff4e893b49c9e3eaecc96209ade666a5ab42101d
MartinMD-Git <mada0008@stud.kea.dk>
2024-11-21 14:08:13 +0100

    mood i main

--- src/Main.java
+++ src/Main.java
@@ -11,6 +11,12 @@ public class Main {
         System.out.println(cat.getName() + " spiser fisk: " + cat.eat("fisk"));
         System.out.println(dog.getName() + " spiser også fisk: " + dog.eat("fisk"));
 
+        dog.showMood(true);
+        dog.showMood(false);
+
+        cat.showMood(true);
+        cat.showMood(false);
+
 
 
     }
